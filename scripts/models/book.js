0c7c9217c2dcd2bf7a9b1bc501192d3e520a8067
Jay Silvas <jayinlava@gmail.com>
2018-04-20 00:58:20 -0700

    Updated Book.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,7 +5,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://jay-judah-book-app.herokuapp.com/';
+ENV.productionApiUrl = 'https://jay-judah-book-list.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
